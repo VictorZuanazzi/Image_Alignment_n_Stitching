db7aac6c918fa12d2f071c66279e8eaa36841f0a
Victor <victorzuanazzi@gmail.com>
2019-03-07 19:10:31 +0100

    maybe correct rotation

--- num_inliers.m
+++ num_inliers.m
@@ -2,12 +2,15 @@ function [inlier_c] = num_inliers(threshold, f1_sel, f2_sel, x)
 
 x = reshape(x, [2, 3]);
 x = [x ; 0 0 1];
-
-f1_sel = [f1_sel; ones(1, size(f1_sel, 2))];
-f1_rot = x*f1_sel;
-f1_rot = f1_rot(1:2,:);
-
-distance = vecnorm(f1_rot - f2_sel);
+% disp(x)
+f2_sel = [f2_sel; ones(1, size(f2_sel, 2))];
+f2_rot = x*f2_sel;
+f2_rot = f2_rot(1:2,:);
+% disp(f1_sel)
+% disp(f2_rot)
+% disp(f2_sel)
+distance = vecnorm(f2_rot - f1_sel);
+% disp(distance)
 
 inlier_c = sum(distance < threshold);
 end
