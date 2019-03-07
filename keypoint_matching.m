501562599b4f286c01468546fb8ea6114a7c807a
Victor <victorzuanazzi@gmail.com>
2019-03-07 12:13:30 +0100

    clean and commented

--- keypoint_matching.m
+++ keypoint_matching.m
@@ -1,28 +1,20 @@
-function [matches] = keypoint_matching(I1, I2, method)
+function [matches, f1, f2] = keypoint_matching(I1, I2)
 % Takes two imageages and return the keypoint matches between them.
 % Inputs: I1, I2 are 2d images.
-% Output: matches is a matrix containing the pixels coordinates(?) of the
-% matching points. Shouldn't we also return the descriptors?
+% Output: 
+    %matches: matrix 2 x n, containing the indexes of the matching features 
+    % in two given figures.
+    %f1, f2: matching features.
+  
+    %tutorial from vlfeat:
+    %convert to single precision.
+    I1 = single(I1);
+    I2 = single(I2);
 
-switch lower(method)
-    case 'matlab'
-        %that is not what they wanted, I think
-        corners1 = detectFASTFeatures(I1, 'MinContrast', 0.3);
-        corners2 = detectFASTFeatures(I2, 'MinContrast', 0.3);
-        [features1,valid_points1] = extractFeatures(I1,corners1);
-        [features2,valid_points2] = extractFeatures(I2,corners2);
-        matches = matchFeatures(features1,features2);
-    
-    case 'vl'
-        %tutorial from vlfeat:
-        %convert to single precision.
-        I1 = single(I1);
-        I2 = single(I2);
-
-        %compute SIFT keypoints.
-        [f1, d1] = vl_sift(I1);
-        [f2, d2] = vl_sift(I2);
-        [matches, scores] = vl_ubcmatch(d1, d2);
+    %compute SIFT keypoints.
+    [f1, d1] = vl_sift(I1);
+    [f2, d2] = vl_sift(I2);
+    [matches, scores] = vl_ubcmatch(d1, d2);
 end
 
 
